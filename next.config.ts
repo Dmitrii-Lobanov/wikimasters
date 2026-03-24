ef91352d1a4d2540c29c24dd61d2191215a19318
Dmitrii <dmitriilobanov3@gmail.com>
2026-03-24 15:14:09 -0500

    UI Styling

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
 import { dirname } from "node:path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: dirname(__filename),
-  }
+  },
 };
 
 export default nextConfig;
