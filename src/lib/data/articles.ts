678ab20d6d029fa136498a0bcd2c46b82ddcaf57
Dmitrii <dmitriilobanov3@gmail.com>
2026-04-01 08:59:51 -0500

    Fix

--- src/lib/data/articles.ts
+++ src/lib/data/articles.ts
@@ -17,33 +17,42 @@ export type ArticleList = {
 
 export async function getArticles(): Promise<ArticleList[]> {
   const cached = await redis.get<ArticleList[]>("articles:all");
+
   if (cached) {
     console.log("🎯 Get Articles Cache Hit!");
     return cached;
   }
 
-  const response = await db
-    .select({
-      title: articles.title,
-      id: articles.id,
-      createdAt: articles.createdAt,
-      summary: articles.summary,
-      content: articles.content,
-      author: usersSync.name,
-    })
-    .from(articles)
-    .leftJoin(usersSync, eq(articles.authorId, usersSync.id));
-
-  console.log("🏹 Get Articles Cache Miss!");
-  // Store cache as JSON so we can retrieve a typed array later
   try {
-    await redis.set("articles:all", JSON.stringify(response), {
-      ex: 60,
-    });
+    const response = await db
+      .select({
+        title: articles.title,
+        id: articles.id,
+        createdAt: articles.createdAt,
+        summary: articles.summary,
+        content: articles.content,
+        author: usersSync.name,
+      })
+      .from(articles)
+      .leftJoin(usersSync, eq(articles.authorId, usersSync.id));
+
+    console.log("🏹 Get Articles Cache Miss!");
+
+    // Store cache as JSON so we can retrieve a typed array later
+    try {
+      await redis.set("articles:all", JSON.stringify(response), {
+        ex: 60,
+      });
+    } catch (err) {
+      console.warn("Failed to set articles cache", err);
+    }
+
+    return response as unknown as ArticleList[];
   } catch (err) {
-    console.warn("Failed to set articles cache", err);
+    console.warn("Failed to get articles", err);
   }
-  return response as unknown as ArticleList[];
+
+  return [];
 }
 
 export type ArticleWithAuthor = {
